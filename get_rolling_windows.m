2048a0a871c4f94e780afa2e73199c4938ce5088
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-03-08 14:12:34 +0100

    Add files via upload

---
+++ get_rolling_windows.m
@@ -0,0 +1,28 @@
+% [INPUT]
+% ret = A t-by-n matrix containing firms log returns.
+% bw  = The bandwidth (dimension) of each rolling window.
+%
+% [OUTPUT]
+% win = A column vector containing the rolling windows.
+%
+% [NOTE]
+% If the number of observations is less than or equal to the specified bandwidth, a single rolling window containing all the observations is returned.
+
+function win = get_rolling_windows(ret,bw)
+
+    t = length(ret);
+    
+    if (bw >= t)
+        win = cell(1,1);
+        win{1} = ret;
+        return;
+    end
+
+    lim = t - bw + 1;
+    win = cell(lim,1);
+
+	for i = 1:lim
+        win{i} = ret(i:bw+i-1,:);
+	end
+
+end
