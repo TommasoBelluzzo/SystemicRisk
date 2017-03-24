9402c39fd8fbf48e2c769e122ef4eed79e0a4d59
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-03-24 15:16:58 +0100

    Update calculate_covar.m

--- ScriptsProbabilistic/calculate_covar.m
+++ ScriptsProbabilistic/calculate_covar.m
@@ -1,6 +1,6 @@
 % [INPUT]
-% r0_m   = A column vector containing the demeaned market index log returns.
-% r0_x   = A column vector containing the demeaned firm log returns.
+% ret0_m = A column vector containing the demeaned market index log returns.
+% ret0_x = A column vector containing the demeaned firm log returns.
 % s_x    = A column vector containing the volatilities of the firm log returns.
 % k      = A scalar [0,1] representing the confidence level.
 % svars  = A t-by-n matrix containing the lagged state variables (optional, default=[]).
@@ -16,17 +16,17 @@
 % covar  = A column vector containing the CoVaR values.
 % dcovar = A column vector containing the Delta CoVaR values.
 
-function [covar,dcovar] = calculate_covar(r0_m,r0_x,s_x,k,svars)
+function [covar,dcovar] = calculate_covar(ret0_m,ret0_x,s_x,k,svars)
 
-    t = length(r0_m);
+    t = length(ret0_m);
 
-    var_x = s_x * quantile((r0_x ./ s_x),k);
+    var_x = s_x * quantile((ret0_x ./ s_x),k);
 
     if (nargin < 5)
-        [beta,~,~,~] = quantile_regression(r0_m,r0_x,k);
+        [beta,~,~,~] = quantile_regression(ret0_m,ret0_x,k);
         covar = (beta(1) + (beta(2) .* var_x)) .* -1;
     else
-        [beta,~,~,~] = quantile_regression(r0_m,[r0_x svars],k);
+        [beta,~,~,~] = quantile_regression(ret0_m,[ret0_x svars],k);
         covar = beta(1) + (beta(2) .* var_x);
 
         svars_cnt = size(svars,2);
@@ -38,6 +38,6 @@ function [covar,dcovar] = calculate_covar(r0_m,r0_x,s_x,k,svars)
         covar = covar .* -1;
     end
 
-    dcovar = (beta(2) .* (var_x - repmat(median(r0_x),t,1))) .* -1;
+    dcovar = (beta(2) .* (var_x - repmat(median(ret0_x),t,1))) .* -1;
 
 end
