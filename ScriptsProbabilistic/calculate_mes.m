2dc7a62d22385c22554bdca3d9bb475f430f170b
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-03-24 02:36:52 +0100

    Update calculate_mes.m

--- ScriptsProbabilistic/calculate_mes.m
+++ ScriptsProbabilistic/calculate_mes.m
@@ -1,23 +1,23 @@
 % [INPUT]
-% rdem_m = A column vector containing the demeaned market index log returns.
-% s_m    = A column vector containing the volatilities of the market index log returns.
-% rdem_x = A column vector containing the demeaned firm log returns.
-% s_x    = A column vector containing the volatilities of the firm log returns.
-% p_mx   = A column vector containing the DCC coefficients.
-% k      = A scalar [0,1] representing the confidence level.
+% r0_m  = A column vector containing the demeaned market index log returns.
+% s_m   = A column vector containing the volatilities of the market index log returns.
+% r0_x  = A column vector containing the demeaned firm log returns.
+% s_x   = A column vector containing the volatilities of the firm log returns.
+% p_mx  = A column vector containing the DCC coefficients.
+% k     = A scalar [0,1] representing the confidence level.
 %
 % [OUTPUT]
-% mes    = A column vector containing the MES values.
-% lrmes  = A column vector containing the Long Run MES (LRMES) approximated values.
+% mes   = A column vector containing the MES values.
+% lrmes = A column vector containing the Long Run MES (LRMES) approximated values.
 
-function [mes,lrmes] = calculate_mes(rdem_m,s_m,rdem_x,s_x,p_mx,k)
+function [mes,lrmes] = calculate_mes(r0_m,s_m,r0_x,s_x,p_mx,k)
 
-    c = quantile(rdem_m,k);
-    h = 1 * (length(rdem_m) ^ (-0.2));
-    u = rdem_m ./ s_m;
+    c = quantile(r0_m,k);
+    h = 1 * (length(r0_m) ^ (-0.2));
+    u = r0_m ./ s_m;
 
     x_den = sqrt(1 - (p_mx .^ 2));
-    x_num = (rdem_x ./ s_x) - (p_mx .* u);
+    x_num = (r0_x ./ s_x) - (p_mx .* u);
     x = x_num ./ x_den;
 
     f = normcdf(((c ./ s_m) - u) ./ h);
