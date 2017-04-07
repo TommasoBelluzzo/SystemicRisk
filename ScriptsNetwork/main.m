14cdad5daf8742ad0b372f03cc907d752721fb35
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-04-07 14:23:50 +0200

    Add files via upload

--- ScriptsNetwork/main.m
+++ ScriptsNetwork/main.m
@@ -1,27 +1,55 @@
 % [INPUT]
-% file_src = The file containing the dataset.
-% file_des = The name of the file to which the results are written.
+% file_src = A string representing the name of the Excel spreadsheet containing the dataset (optional, default='dataset.xlsx').
+% file_des = A string representing the name of the Excel spreadsheet to which the results are written, eventually replacing the previous ones (optional, default='results.xlsx').
+% sst      = A float representing he statistical significance threshold for the linear Granger-causality test (optional, default=0.05).
+% rob      = A boolean indicating whether to use robust p-values (optional, default=true).
+% anl      = A boolean that indicates whether to analyse the results (optional, default=false).
 %
-% [NOTE]
-% The financial time series in the dataset must have been previously validated and preprocessed:
-%  - there are enough observations to run consistent calculations;
-%  - illiquid series with too many zeroes have been discarded;
-%  - outliers have been detected and removed;
-%  - rows with NaNs have been removed or filled with interpolation;
-%  - etc...
+% [NOTES]
+% This function produces no outputs, its purpose is to save the results into an Excel spreadsheet and, optionally, analyse them.
 
-function main(file_src,file_des)
+function main(varargin)
 
-    if (exist(file_src,'file') == 0)
-        error('The source file does not exist.');
+    persistent ip;
+
+    if (isempty(ip))
+        ip = inputParser();
+        ip.addOptional('file_src','dataset.xlsx',@(x)validateattributes(x,{'char'},{'nonempty','size',[1,NaN]}));
+        ip.addOptional('file_des','results.xlsx',@(x)validateattributes(x,{'char'},{'nonempty','size',[1,NaN]}));
+        ip.addOptional('sst',0.05,@(x)validateattributes(x,{'double','single'},{'scalar','real','finite','>',0,'<=',0.20}));
+        ip.addOptional('rob',true,@(x)validateattributes(x,{'logical'},{'scalar'}));
+        ip.addOptional('anl',false,@(x)validateattributes(x,{'logical'},{'scalar'}));
     end
 
-    grp = [5; 11; 18];
-    rob = 1;
-    sst = 0.05;
+    ip.parse(varargin{:});
+    ip_res = ip.Results;
+    
+    [path,~,~] = fileparts(pwd);
+    [~,name,ext] = fileparts(ip_res.file_src);
+	file_src = fullfile(path,[name ext]);
 
-    ret = get_firms_returns(file_src);
-    win = get_rolling_windows(ret,252);
+    [~,name,ext] = fileparts(ip_res.file_des);
+    
+    if (~strcmp(ext,'.xlsx'))
+        file_des = fullfile(pwd,[name ext '.xlsx']);
+    else
+        file_des = fullfile(pwd,[name ext]); 
+    end
+    
+    main_internal(file_src,file_des,ip_res.sst,ip_res.rob,ip_res.anl);
+
+end
+
+function main_internal(file_src,file_des,sst,rob,anl)
+
+    addpath('../ScriptsCommon');
+    data = parse_dataset(file_src);
+    rmpath('../ScriptsCommon');
+
+    data.SST = sst;
+    data.Rob = rob;
+
+    win = get_rolling_windows(data.RetFrms,252);
     win_len = length(win);
     
     res = cell(win_len,1);
---
+++ ScriptsNetwork/run.m
@@ -0,0 +1,7 @@
+warning off all;
+
+close all;
+clearvars;
+clc;
+
+main('dataset.xlsx','results.xlsx',0.05,true,true);
