0c0d6cc56d8a48eadc0ae21b047b0af93a802b40
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-04-10 16:40:45 +0200

    Update run.m

--- run.m
+++ run.m
@@ -11,7 +11,7 @@ addpath(paths);
 
 data = parse_dataset('dataset.xlsx');
 
-main_pro(data,fullfile(path,'results_pro.xlsx'),0.95,0.08,true);
+main_pro(data,fullfile(path,'results_pro.xlsx'),0.95,0.40,0.08,true);
 main_net(data,fullfile(pwd,'results_net.xlsx'),0.05,true,true);
 
 save('data.mat','data');
