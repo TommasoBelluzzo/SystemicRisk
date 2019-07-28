9eb4f5994f75997e6aa4df6f82d25bc4b4d884ef
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-07-28 13:26:11 +0200

    Add files via upload

--- run.m
+++ run.m
@@ -12,26 +12,40 @@ if (~strcmpi(path_base(end),filesep()))
 end
 
 paths_base = genpath(path_base);
+paths_base = strsplit(paths_base,';');
+
+for i = numel(paths_base):-1:1
+    path_cur = paths_base{i};
+
+    if (~strcmp(path_cur,path_base) && isempty(regexpi(path_cur,[filesep() 'Scripts'])))
+        paths_base(i) = [];
+    end
+end
+
+paths_base = [strjoin(paths_base,';') ';'];
 addpath(paths_base);
 
-path_dset = strrep('Datasets\Example_Large.xlsx','\',filesep());
+try
+    path_dset = strrep('Datasets\Example_Large.xlsx','\',filesep());
 
-path_tpro = strrep('Templates\TemplatePRO.xlsx','\',filesep());
-file_tpro = fullfile(path_base,path_tpro);
-path_rpro = strrep('Results\ResultsPRO.xlsx','\',filesep());
-file_rpro = fullfile(path_base,path_rpro);
+    path_tpro = strrep('Templates\TemplatePRO.xlsx','\',filesep());
+    file_tpro = fullfile(path_base,path_tpro);
+    path_rpro = strrep('Results\ResultsPRO.xlsx','\',filesep());
+    file_rpro = fullfile(path_base,path_rpro);
 
-path_tnet = strrep('Templates\TemplateNET.xlsx','\',filesep());
-file_tnet = fullfile(path_base,path_tnet);
-path_rnet = strrep('Results\ResultsNET.xlsx','\',filesep());
-file_rnet = fullfile(path_base,path_rnet);
+    path_tnet = strrep('Templates\TemplateNET.xlsx','\',filesep());
+    file_tnet = fullfile(path_base,path_tnet);
+    path_rnet = strrep('Results\ResultsNET.xlsx','\',filesep());
+    file_rnet = fullfile(path_base,path_rnet);
+    
+    data = parse_dataset(fullfile(path_base,path_dset));
 
-data = parse_dataset(fullfile(path_base,path_dset));
+    main_pro(data,file_tpro,file_rpro,0.95,0.40,0.08,true);
+    pause(2);
+    main_net(data,file_tnet,file_rnet,0.05,true,true);
 
-main_pro(data,file_tpro,file_rpro,0.95,0.40,0.08,true);
-pause(2);
-main_net(data,file_tnet,file_rnet,0.05,true,true);
-
-save('data.mat','data');
-
-rmpath(paths_base);
+    save('data.mat','data');
+catch e
+   rmpath(paths_base);
+   rethrow(e);
+end
